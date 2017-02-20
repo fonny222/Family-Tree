c5286e7286b142d0635ab638def47d0dae2c5f4f
fonny222 <christopherdfontana@gmail.com>
2017-02-19 19:44:41 -0600

    navigation family tree
    used a navigation control embed  view controller in nav, buttons to
    create a family tree application

---
+++ Family Tree/grandParentsViewController.swift
@@ -0,0 +1,42 @@
+//
+//  grandParentsViewController.swift
+//  Family Tree
+//
+//  Created by Christopher Fontana on 2/19/17.
+//  Copyright © 2017 Christopher Fontana. All rights reserved.
+//
+
+import UIKit
+
+
+// i created the button from the previous view and click dragged it to the next view controller and used "show" segue
+// this moves the stack from the one before to the new one
+
+// using the properties you can add a back button to the screen and label it what you want. 
+// you have to drag a navigation item to each new view controller after the initial one. 
+
+class grandParentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
